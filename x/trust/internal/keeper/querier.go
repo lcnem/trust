5f70f70178184c4497285c5688bbb1407c80efe4
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-09-30 08:28:53 +0900

    [update]querier

--- x/trust/internal/keeper/querier.go
+++ x/trust/internal/keeper/querier.go
@@ -40,10 +40,7 @@ func queryAccountScores(ctx sdk.Context, path []string, req abci.RequestQuery, k
 	vector := keeper.GetAccountScores(ctx, topicIDs, address)
 	bz := codec.MustMarshalJSONIndent(keeper.cdc, vector)
 
-	res, err := codec.MarshalJSONIndent(keeper.cdc, types.QueryResAccountScores{Scores: string(bz)})
-	if err != nil {
-		panic("could not marshal result to JSON")
-	}
+	res := codec.MustMarshalJSONIndent(keeper.cdc, types.QueryResAccountScores{Scores: string(bz)})
 
 	return res, nil
 }
