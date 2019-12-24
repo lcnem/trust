ff8ea28cfcf09b89e28d28de2dd62ce23ae62688
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-12-24 10:10:06 +0900

    [update]underbar

--- x/trust/internal/types/msgs.go
+++ x/trust/internal/types/msgs.go
@@ -161,5 +161,5 @@ func (msg MsgDistributeTokenByEvaluation) GetSigners() []sdk.AccAddress {
 }
 
 func validateTopicID(topicID string) bool {
-	return regexp.MustCompile("^[a-z]([a-z0-9]|-)*$").Match([]byte(topicID))
+	return regexp.MustCompile("^[a-z_][a-z_0-9]*$").Match([]byte(topicID))
 }
